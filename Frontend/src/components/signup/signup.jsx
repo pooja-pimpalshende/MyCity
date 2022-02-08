b72053aa997742a780e31898da11d773e2d2d60d
pooja-pim <pooja.pimpalshende215@gmail.com>
2022-02-08 13:30:14 +0530

    signupComponent

--- Frontend/src/components/signup/signup.jsx
+++ Frontend/src/components/signup/signup.jsx
@@ -1,1 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './signup.css'
+
+function SignUpComponent(){
+    return(
+        <div>
+            <form>
+                Name : <input text="text" name="username"></input>
+                Password : <input text="text" name="password"></input>
+                Email : <input text="email" name="email"></input>
+                Mobile : <input text="number" name="phone"></input>
+                <button>Sign Up</button>
+
+            </form>
+            
+        </div>
+    )
+
+}
+
+export default SignUpComponent
