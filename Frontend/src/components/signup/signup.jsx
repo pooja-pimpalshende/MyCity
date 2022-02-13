10f1b19aa4a47fac657dce96cc69176974945922
Krutuja <rutu.rekhe1999@gmail.com>
2022-02-13 17:29:23 +0530

    changes in signup css

--- Frontend/src/components/signup/signup.jsx
+++ Frontend/src/components/signup/signup.jsx
@@ -3,13 +3,13 @@ import './signup.css'
 
 function SignUpComponent(){
     return(
-        <div>
-            <form>
-                Name : <input text="text" class="input-box" name="username"></input>
-                Password : <input text="text" class="input-box" name="password"></input>
-                Email : <input text="email" class="input-box" name="email"></input>
-                Mobile : <input text="number" class="input-box" name="phone"></input>
-                <button type="button" class="btn">Sign Up</button>
+        <div class="form">
+		<form>
+			Name : <input text="text" placeholder="username"/>
+                Password : <input text="text" placeholder="password"/>
+                Email : <input text="email" placeholder="email"/>
+                Mobile : <input text="number" placeholder="mobile"/>
+                <button>Sign Up</button>
 
             </form>
             
