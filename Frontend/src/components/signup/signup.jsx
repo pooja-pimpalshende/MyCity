1d2dbda70fd1e4f44d34f80ab38c70d5ec19d382
Krutuja <rutu.rekhe1999@gmail.com>
2022-02-13 14:02:13 +0530

    signup css

--- Frontend/src/components/signup/signup.jsx
+++ Frontend/src/components/signup/signup.jsx
@@ -5,11 +5,11 @@ function SignUpComponent(){
     return(
         <div>
             <form>
-                Name : <input text="text" name="username"></input>
-                Password : <input text="text" name="password"></input>
-                Email : <input text="email" name="email"></input>
-                Mobile : <input text="number" name="phone"></input>
-                <button>Sign Up</button>
+                Name : <input text="text" class="input-box" name="username"></input>
+                Password : <input text="text" class="input-box" name="password"></input>
+                Email : <input text="email" class="input-box" name="email"></input>
+                Mobile : <input text="number" class="input-box" name="phone"></input>
+                <button type="button" class="btn">Sign Up</button>
 
             </form>
             
