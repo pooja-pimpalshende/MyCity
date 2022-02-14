adc286e33c2a67d23a1bf0519e612f04a07fb796
pooja-pim <pooja.pimpalshende215@gmail.com>
2022-02-14 10:02:42 +0530

    Added hostel component,library component

--- Frontend/src/components/home/home.jsx
+++ Frontend/src/components/home/home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import './home.css'
+import SignUpComponent from "../signup/signup";
 
 function HomeComponent() {
     return (
-        <div>This is Home</div>);
+        
+        <div>
+            {/* <SignUpComponent /> */}
+            This is Home
+        </div>);
   }
 
   export default HomeComponent
--- Frontend/src/components/library/library.jsx
+++ Frontend/src/components/library/library.jsx
@@ -1,11 +1,120 @@
-import React from 'react';
+import React from "react";
+import "./library.css";
+import library1 from "../../assets/images/library1.jpg";
+import library2 from "../../assets/images/library2.jpg";
+import library3 from "../../assets/images/library3.jpg";
+import library4 from "../../assets/images/library4.jpg";
+import Button from "react-bootstrap/Button";
 
 function LibraryComponent() {
-    return (
+  return (
+    <div className="container-library">
+      <div className="left">
       <div>
-        This is Library page
+      <button type="button" class="btn btn-secondary">Public Library</button>
+        
+        <button type="button" class="btn btn-secondary">Government Library</button>
       </div>
-    );
-  }
+      </div>
+      <div className="right">
+        <div className="searchBy">
+          <input text="text" id="search" placeholder="Search By Name" />
+        </div>
+        <div className="dropdown-style">
+          <div class="dropdown">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+        </div>
 
-  export default LibraryComponent
+        <div className="libraryBox">
+          <div className="library-info">
+            <div>
+              <img src={library1} />
+            </div>
+
+            <div>
+              <h1>heading </h1>
+              <p>
+                <a href="">Click Here For More Info</a>
+              </p>
+            </div>
+            <div>
+              <button type="button" class="btn btn-success">
+              Enquire Now
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* <div className="libraryBox">
+          <div className="library-info">
+            <div>
+              <img src={library2} />
+            </div>
+            <div>
+              <h1>heading </h1>
+              <p>
+                <a href="">Click Here For More Info</a>
+              </p>
+            </div>
+            <div>
+              <button>Enquire Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="libraryBox">
+          <div className="library-info">
+            <div>
+              <img src={library3} />
+            </div>
+            <div>
+              <h1>heading </h1>
+              <p>
+                <a href="">Click Here For More Info</a>
+              </p>
+            </div>
+            <div>
+              <button>Enquire Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="libraryBox">
+          <div className="library-info">
+            <div>
+              <img src={library4} />
+            </div>
+            <div>
+              <h1>heading </h1>
+              <p>
+                <a href="">Click Here For More Info</a>
+              </p>
+            </div>
+            <div>
+              <button>Enquire Now</button>
+            </div> */}
+        {/* </div> */}
+        {/* </div> */}
+      </div>
+    </div>
+  );
+}
+export default LibraryComponent;
